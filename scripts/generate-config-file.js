e8e10aa41f38999b46c72450a8987f47c4b67da0
Leonardo Vieira <leovieira20@gmail.com>
2017-10-21 18:03:09 +0100

    generating config files from server

---
+++ scripts/generate-config-file.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+
+if (!process.env.ENV_TYPE) {
+  throw new Error('No ENV_TYPE');
+}
+
+if (!process.env.FACEBOOK_APP_ID) {
+  throw new Error('No FACEBOOK_APP_ID');
+}
+
+if (!process.env.APP_ID) {
+  throw new Error('No APP_ID');
+}
+
+if (!process.env.SERVER_URL) {
+  throw new Error('No SERVER_URL');
+}
+
+var isProduction = process.env.ENV_TYPE === 'PROD';
+
+var confFile = "export const environment = {\n" +
+  "  production: " + isProduction + ",\n" +
+  "  facebookAppId: '" + process.env.FACEBOOK_APP_ID + "',\n" +
+  "  parseAppId: '" + process.env.APP_ID + "',\n" +
+  "  parseUrl: '" + process.env.SERVER_URL + "'\n" +
+  "};";
+
+fs.writeFile('src/environments/environment.' + process.env.ENV_TYPE + '.ts', confFile, function (err) {
+  if (err) {
+    throw err;
+  }
+});
