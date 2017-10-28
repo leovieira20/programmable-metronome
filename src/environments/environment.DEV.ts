47bc9eb4b4d0dd34212e5a054ce43fe2e9d52cd5
Leonardo Vieira <leovieira20@gmail.com>
2017-10-28 19:32:15 +0100

    update git.ignore to remove dynamic generated files

--- src/environments/environment.DEV.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  facebookAppId: '530985340571324',
-  parseAppId: '2325F541-0A49-490D-952E-05CE349E487F',
-  parseUrl: 'https://pgmb-metronome.herokuapp.com/parse'
-};
